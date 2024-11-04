fb515b6bd6a57743d7c9664abd224f73ecb4b70e
sourav-coleb <paul2305101956@diu.edu.bd>
2024-11-04 21:49:39 +0600

    modified the user structure and create a registration folder and file

--- eventEase.sql
+++ eventEase.sql
@@ -8,6 +8,7 @@
 --     firstname VARCHAR(100) NOT NULL,
 --     lastname VARCHAR(100) NOT NULL,
 --     email VARCHAR(255) UNIQUE NOT NULL,
+--     mobileNumber VARCHAR(15),
 --     password VARCHAR(255) NOT NULL, 
 --     age INT CHECK (age >= 18),
 --     role ENUM('admin', 'user') NOT NULL default('user'), 
