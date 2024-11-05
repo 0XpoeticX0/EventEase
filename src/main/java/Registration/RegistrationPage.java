b2572606a731e8703793da3ff37540e01ab16b58
sourav-coleb <paul2305101956@diu.edu.bd>
2024-11-05 10:00:34 +0600

    import needed files, add class and methods

---
+++ src/main/java/Registration/RegistrationPage.java
@@ -0,0 +1,23 @@
+package Registration;
+
+import javax.swing.*;
+// import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+// import java.io.File;
+
+public class RegistrationPage extends JFrame implements ActionListener {
+
+    
+    public RegistrationPage() {
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
