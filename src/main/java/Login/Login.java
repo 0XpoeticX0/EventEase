28b301787c9c6b3423044b4f5e4b78349013b4fe
souravghosh203 <kumar23105101296@diu.edu.bd>
2024-11-05 23:13:18 +0600

    Frame and packages added

--- src/main/java/Login/Login.java
+++ src/main/java/Login/Login.java
@@ -1,5 +1,15 @@
 package Login;
 
-public class Login {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Login extends JFrame {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+    private JLabel registerLabel;
 }
