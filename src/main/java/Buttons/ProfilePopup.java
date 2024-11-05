d6cd1b45beb57cced7f2db1d8750dd36d49d65f9
0XpoeticX0 <ashfikur.rahaman2018@gmail.com>
2024-11-05 17:31:52 +0600

    Added HeaderPanel

--- src/main/java/Buttons/ProfilePopup.java
+++ src/main/java/Buttons/ProfilePopup.java
@@ -1,22 +1,22 @@
-package Buttons;
-
-import java.awt.Component;
-import javax.swing.*;
-
-public class ProfilePopup extends JPopupMenu {
-    public ProfilePopup(Component parent) {
-        JMenuItem logoutMenuItem = new JMenuItem("Logout");
-        JMenuItem recordsMenuItem = new JMenuItem("Records");
-
-        logoutMenuItem.addActionListener(e -> {
-            JOptionPane.showMessageDialog(parent, "Logged out successfully.");
-        });
-
-        recordsMenuItem.addActionListener(e -> {
-            JOptionPane.showMessageDialog(parent, "Displaying records.");
-        });
-
-        add(recordsMenuItem);
-        add(logoutMenuItem);
-    }
-}
+//package Buttons;
+//
+//import java.awt.Component;
+//import javax.swing.*;
+//
+//public class ProfilePopup extends JPopupMenu {
+//    public ProfilePopup(Component parent) {
+//        JMenuItem logoutMenuItem = new JMenuItem("Logout");
+//        JMenuItem recordsMenuItem = new JMenuItem("Records");
+//
+//        logoutMenuItem.addActionListener(e -> {
+//            JOptionPane.showMessageDialog(parent, "Logged out successfully.");
+//        });
+//
+//        recordsMenuItem.addActionListener(e -> {
+//            JOptionPane.showMessageDialog(parent, "Displaying records.");
+//        });
+//
+//        add(recordsMenuItem);
+//        add(logoutMenuItem);
+//    }
+//}
