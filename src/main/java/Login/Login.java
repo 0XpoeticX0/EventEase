2af3ab48c562f28916e6642849f7efba47e0ea27
souravghosh203 <kumar23105101296@diu.edu.bd>
2024-11-06 17:35:40 +0600

    Title and size defined

--- src/main/java/Login/Login.java
+++ src/main/java/Login/Login.java
@@ -12,4 +12,11 @@ public class Login extends JFrame {
     private JPasswordField passwordField;
     private JButton loginButton;
     private JLabel registerLabel;
+
+    public Login() {
+        setTitle("Colorful Login Interface");
+        setSize(700, 550);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
 }
